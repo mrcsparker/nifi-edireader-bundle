9fce2b0e80c846396a441873e01055f728e444b9
Chris Parker <mrcsparker@gmail.com>
2016-09-15 22:22:12 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# NIFI EDIReader Processor
+
+[Apache NIFI](https://nifi.apache.org) processor that converts
+EDI [ASC X12](https://en.wikipedia.org/wiki/ASC_X12) and
+[EDIFACT](https://en.wikipedia.org/wiki/EDIFACT)
+documents into XML using the
+[EDIReader](https://github.com/BerryWorksSoftware/edireader) library.
+
+## Usage
+
+* [Download Apache NIFI](https://nifi.apache.org/download.html)
+
+* Grab EDIReader from github
+
+`nifi-edireader-processor` uses a new version of EDIReader that is not available in a Maven repo.
+
+```bash
+> git clone https://github.com/BerryWorksSoftware/edireader.git
+> cd edireader
+> mvn install
+```
+
+* Compile and install `nifi-edireader-processor`
+
+```bash
+> cd nifi-edireader-processor
+> mvn package
+> cp nifi-edireader-nar/target/nifi-edireader-nar-0.0.1.nar /NIFI_INSTALL/lib/
+```
+
+## License
+
+The code for this project is licensed under the Apache 2 License.
+
+The code for EDIReader is licensed under the GNU GPL v3.
+
+When used with the GPLv3 version of EDIReader the license for project falls under the GNU GPL v3 license.
+
+
+
---
+++ nifi-edireader-processors/src/main/java/org/apache/nifi/processors/edireader/EDIReaderProcessor.java
@@ -0,0 +1,135 @@
+package org.apache.nifi.processors.edireader;
+
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.processor.*;
+import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.processor.io.StreamCallback;
+import org.apache.nifi.stream.io.ByteArrayOutputStream;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Tags({"EDI", "X12", "XML"})
+@CapabilityDescription("Transform EDI X12 into XML.")
+public class EDIReaderProcessor extends AbstractProcessor {
+
+    private List<PropertyDescriptor> descriptors;
+    private Set<Relationship> relationships;
+
+    public static final Relationship REL_SUCCESS = new Relationship.Builder()
+            .name("success")
+            .description("success")
+            .build();
+
+    public static final Relationship REL_FAILURE = new Relationship.Builder()
+            .name("failure")
+            .description("failure")
+            .build();
+
+    @Override
+    public void init(final ProcessorInitializationContext context) {
+        Set<Relationship> relationships = new HashSet<>();
+        relationships.add(REL_SUCCESS);
+        relationships.add(REL_FAILURE);
+        this.relationships = Collections.unmodifiableSet(relationships);
+    }
+
+    @Override
+    public Set<Relationship> getRelationships() {
+        return this.relationships;
+    }
+
+    @Override
+    public final List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        return this.descriptors;
+    }
+
+    @Override
+    public void onTrigger(ProcessContext processContext, ProcessSession session) throws ProcessException {
+        final FlowFile flowFile = session.get();
+        if (flowFile == null) {
+            return;
+        }
+
+        InputStream input = session.read(flowFile);
+        InputSource inputSource = new InputSource(input);
+
+        OutputStream output = new ByteArrayOutputStream();
+
+        try {
+
+            // Establish an XMLReader which is actually an EDIReader.
+            System.setProperty("javax.xml.parsers.SAXParserFactory","com.berryworks.edireader.EDIParserFactory");
+            SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+            SAXParser saxParser = saxParserFactory.newSAXParser();
+            XMLReader xmlReader = saxParser.getXMLReader();
+
+            // Establish the SAXSource
+            SAXSource saxSource = new SAXSource(xmlReader, inputSource);
+
+            // Establish an XSL Transformer to generate the XML output.
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+
+            // The StreamResult to capture the generated XML output.
+            StreamResult streamResult = new StreamResult(output);
+
+            // Call the XSL Transformer with no stylesheet to generate
+            // XML output from the parsed input.
+            transformer.transform(saxSource, streamResult);
+
+
+            FlowFile updatedFlowfile = session.write(flowFile, new StreamCallback() {
+                @Override
+                public void process(InputStream inputStream, OutputStream outputStream) throws IOException {
+                    outputStream.write(output.toString().getBytes());
+                }
+            });
+
+            input.close();
+            output.close();
+
+            session.transfer(updatedFlowfile, REL_SUCCESS);
+            session.commit();
+
+        } catch (SAXException e) {
+            getLogger().error("SAXException: " + e.getMessage());
+            session.transfer(flowFile, REL_FAILURE);
+        } catch (ParserConfigurationException e) {
+            getLogger().error("ParserConfigurationException: " + e.getMessage());
+            session.transfer(flowFile, REL_FAILURE);
+        } catch (TransformerConfigurationException e) {
+            getLogger().error("TransformerConfigurationException: " + e.getMessage());
+            session.transfer(flowFile, REL_FAILURE);
+        } catch (TransformerException e) {
+            getLogger().error("TransformerException: " + e.getMessage());
+            e.printStackTrace();
+            session.transfer(flowFile, REL_FAILURE);
+        } catch (IOException e) {
+            getLogger().error("IOException: " + e.getMessage());
+            session.transfer(flowFile, REL_FAILURE);
+        }
+    }
+}
